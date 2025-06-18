4ee32068e879a424bb493385efe3e3df09a0b5c4
Cameron Cowen <cameronc94@gmail.com>
2025-06-19 00:22:07 +0100

    initial rework of nunit converter

--- src/MilkyWare.Sarif.Converter/Converters/NUnitConverter.cs
+++ src/MilkyWare.Sarif.Converter/Converters/NUnitConverter.cs
@@ -1,50 +1,58 @@
-﻿using System.Xml.Linq;
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.Extensions.Logging;
 using MilkyWare.Sarif.Converter.Enums;
 
 namespace MilkyWare.Sarif.Converter.Converters
 {
-    public class NUnitConverter : ISarifConverter
+    public class NUnitConverter(ILogger<NUnitConverter> logger) : ISarifConverter
     {
+        private readonly ILogger<NUnitConverter> _logger = logger;
+
         public FormatType FormatType => FormatType.NUnit;
 
         public async Task<string> ConvertAsync(SarifLog sarif)
         {
-            var testCases = new List<XElement>();
+            var resultsCount = sarif.Runs.SelectMany(r => r.Results)
+                .Count();
+            var testRun = new XElement("test-run",
+                new XAttribute("testcasecount", resultsCount),
+                new XAttribute("total", resultsCount),
+                new XAttribute("failed", resultsCount),
+                new XAttribute("result", "Failed"));
+
+            var testSuite = new XElement("test-suite");
+            testRun.Add(testSuite);
+
             foreach (var run in sarif.Runs)
             {
                 foreach (var result in run.Results)
                 {
                     var testCase = new XElement("test-case",
-                        new XAttribute("name", result.RuleId),
+                        new XAttribute("name", result.Message.Text),
+                        new XAttribute("classname", result.RuleId),
                         new XAttribute("result", "Failed"));
 
-                    var properties = new List<XElement>();
-                    if (!string.IsNullOrWhiteSpace(result.Message.Text))
-                    {
-                        properties.Add(new XElement("property",
-                            new XAttribute("name", "Description"),
-                            new XAttribute("value", result.Message.Text)));
-                    }
-
-                    if (properties.Any())
-                    {
-                        testCases.Add(new XElement("properties",
-                            new XElement("property", properties)));
-                    }
-                    testCases.Add(testCase);
+                    testSuite.Add(testCase);
                 }
             }
-            var testSuite = new XElement("test-suite");
-            testSuite.Add(testCases);
 
-            var testResults = new XElement("test-results");
-            testResults.SetAttributeValue("result", "Failed");
-            testResults.SetAttributeValue("total", testCases.Count);
-            testResults.SetAttributeValue("failed", testCases.Count);
-            testResults.Add(testSuite);
+            using var ms = new MemoryStream();
+            using var xw = XmlWriter.Create(ms, new()
+            {
+                Async = true,
+                OmitXmlDeclaration = true,
+                Indent = true
+            });
+            await testRun.SaveAsync(xw, default);
+            await xw.FlushAsync();
 
-            return testResults.ToString();
+            ms.Position = 0;
+            var sr = new StreamReader(ms, Encoding.UTF8);
+            var xml = await sr.ReadToEndAsync();
+            return xml;
         }
     }
 }
---
+++ tests/MilkyWare.Sarif.ConverterTests/Converters/NUnitConverterTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.Extensions.Logging;
+
+namespace MilkyWare.Sarif.Converter.Converters.Tests
+{
+    public class NUnitConverterTests
+    {
+        private readonly NUnitConverter _converter;
+        private readonly ILogger<NUnitConverter> _logger;
+
+        public NUnitConverterTests()
+        {
+            _logger = Substitute.For<ILogger<NUnitConverter>>();
+            _converter = new NUnitConverter(_logger);
+        }
+
+        [Fact()]
+        public async Task ConvertAsyncTest()
+        {
+            // Arrange
+            using var ms = new MemoryStream();
+            using var sw = new StreamWriter(ms);
+            await sw.WriteAsync("""
+                                {
+                    "$schema": "https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.6.json",
+                    "version": "2.1.0",
+                    "runs": [
+                        {
+                            "tool": {
+                                "driver": {
+                                    "name": "bicep"
+                                }
+                            },
+                            "results": [
+                                {
+                                    "ruleId": "outputs-should-not-contain-secrets",
+                                    "message": {
+                                        "text": "Outputs should not contain secrets. Found possible secret: function 'listKeys' [https://aka.ms/bicep/linter/outputs-should-not-contain-secrets]"
+                                    },
+                                    "locations": [
+                                        {
+                                            "physicalLocation": {
+                                                "artifactLocation": {
+                                                    "uri": "file:///C:/Git/milkyware/azure-bicep/./.tmp/storageaccount.bicep"
+                                                },
+                                                "region": {
+                                                    "startLine": 206,
+                                                    "charOffset": 113
+                                                }
+                                            }
+                                        }
+                                    ]
+                                },
+                                {
+                                    "ruleId": "outputs-should-not-contain-secrets",
+                                    "message": {
+                                        "text": "Outputs should not contain secrets. Found possible secret: function 'listKeys' [https://aka.ms/bicep/linter/outputs-should-not-contain-secrets]"
+                                    },
+                                    "locations": [
+                                        {
+                                            "physicalLocation": {
+                                                "artifactLocation": {
+                                                    "uri": "file:///C:/Git/milkyware/azure-bicep/./.tmp/storageaccount.bicep"
+                                                },
+                                                "region": {
+                                                    "startLine": 207,
+                                                    "charOffset": 35
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+                            ],
+                            "columnKind": "utf16CodeUnits"
+                        }
+                    ]
+                }
+                """);
+            await sw.FlushAsync();
+            ms.Position = 0;
+            var input = SarifLog.Load(ms);
+
+            // Act
+            var actual = await _converter.ConvertAsync(input);
+
+            // Assert
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
