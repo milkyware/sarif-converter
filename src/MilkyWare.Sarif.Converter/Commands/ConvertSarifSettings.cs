3b913779cbaacaa59487da2d087cb028a9c0d388
Cameron Cowen <cameronc94@gmail.com>
2025-07-21 21:05:06 +0100

    added descriptions to settings

--- src/MilkyWare.Sarif.Converter/Commands/ConvertSarifSettings.cs
+++ src/MilkyWare.Sarif.Converter/Commands/ConvertSarifSettings.cs
@@ -1,4 +1,5 @@
-﻿using MilkyWare.Sarif.Converter.Enums;
+﻿using System.ComponentModel;
+using MilkyWare.Sarif.Converter.Enums;
 using Spectre.Console.Cli;
 
 namespace MilkyWare.Sarif.Converter.Commands
@@ -6,12 +7,15 @@ namespace MilkyWare.Sarif.Converter.Commands
     public class ConvertSarifSettings : LoggingSettings
     {
         [CommandOption("-f|--format")]
+        [Description("Format to convert to")]
         public FormatType FormatType { get; set; } = FormatType.JUnit;
 
         [CommandOption("-i|--input-file")]
+        [Description("Path to the input SARIF file")]
         public string? InputFile { get; set; }
 
         [CommandOption("-o|--output-file")]
+        [Description("Path to output the converted file to. Outputs to stdout if not specified")]
         public string? OutputFile { get; set; }
     }
 }
